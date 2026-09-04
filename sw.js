8fb42f8d1819f6e87814bdafc008699ded4e83be
z754khvy4k-spec <z754khvy4k@privaterelay.appleid.com>
2026-09-04 00:03:01 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE="living-realm-v4";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
